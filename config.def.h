a8e47c7d21df28f4c56de5f51cfd27f22d97dd57
christianchiarulli <chrisatmachine@gmail.com>
2021-04-06 02:28:42 -0400

    update border width

--- config.def.h
+++ config.def.h
@@ -29,4 +29,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 1;
