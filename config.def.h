cb30cc57b982514f1c0c8aff26bd8d63ad53938b
christianchiarulli <chrisatmachine@gmail.com>
2021-04-06 00:17:23 -0400

    add xyz

--- config.def.h
+++ config.def.h
@@ -3,14 +3,12 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char *fonts[] = { "Hack Regular Nerd Font Complete:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#bbbbbb", "#292d38" },
+	[SchemeSel] = { "#eeeeee", "#80a0c0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
