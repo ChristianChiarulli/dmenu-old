e5554ab0b7c13ec40019d0cb9c2548e6348282b5
Binx <68950943+Binx-Codes@users.noreply.github.com>
2021-07-20 21:44:08 +0400

    customize colors

---
+++ config.h
@@ -0,0 +1,32 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = { "Hack Regular Nerd Font Complete:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#282828" },
+	[SchemeSel] = { "#bbbbbb", "#282828" },
+	[SchemeSelHighlight] = { "#4fc1ff", "#444444" },
+	[SchemeNormHighlight] = { "#4fc1ff", "#292d38" },
+	[SchemeOut] = { "#bbbbbb", "#bbbbbb" },
+	[SchemeHp] = { "#bbbbbb", "#bbbbbb" }
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 1;
